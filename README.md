7ba8d46f046d49c4fceeb72c1c2b9565d533104a
jeremyjacob <github@jeremyjacob.dev>
2022-07-14 21:19:32 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# paycointerview
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/paycointerview)
---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  /* 
+  The aim is to create a simple visualization for weather data from a city input by the user.
+  This enviroment has TS and TailwindCSS, please use them
+  Bonus points if it looks nice, but functionality and code quality is what we're looking for
+
+  API request for London (GET)
+  https://api.weatherapi.com/v1/current.json?key=805b3a193e514483ac9232534221307&q=London&aqi=no
+
+  The only restrictions are that the weather data should not be fetched by the same component that renders the information, and that the response from the API must be given type definitions.
+  */
+</script>
+
+<main class="bg-gray-900 text-white h-full p-10" >
+</main>
